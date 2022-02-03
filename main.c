72fcf3a937d8b40a7a5b016134a4744fd03f6049
khirohas <keihiro84@gmail.com>
2022-02-03 23:50:14 +0900

    main

--- main.c
+++ main.c
@@ -3,17 +3,20 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 int		main(void)
 {
 	int		fd = open("41_no_nl", O_RDONLY);
 	char *ret;
 	size_t i = 0;
-	size_t n = 4;
+	size_t n = 1;
 
 	while (i < n)
 	{
 		ret = get_next_line(fd);
 		printf("ret:%s", ret);
+		printf("ret:%ld", ft_strlen(ret));
+		printf("cmp:%i", strcmp(ret, "01234567890123456789012345678901234567890"));
 		free(ret);
 		i++;
 	}

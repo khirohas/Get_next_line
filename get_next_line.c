0d017c145498e0d5f57aaeee34f07a949098f18e
khirohas <keihiro84@gmail.com>
2022-02-05 04:18:00 +0900

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -17,7 +17,8 @@ static char	*check_save(char *save)
 	char	*line;
 	char	*nlptr;
 
-	if (!(line = ft_strcdup(save, '\n')))
+	line = ft_strcdup(save, '\n');
+	if (line == NULL)
 		return (NULL);
 	nlptr = (ft_strchr(save, '\n') + 1);
 	ft_strlcpy(save, nlptr, ft_strlen(nlptr) + 1);
@@ -31,13 +32,15 @@ static char	*ft_read_buffer(int fd)
 	char	*raw_line;
 	ssize_t	read_ret;
 
-	raw_line	= NULL;
-	if (!(buf1 = malloc(BUFFER_SIZE + 1)))
+	raw_line = NULL;
+	buf1 = malloc(BUFFER_SIZE + 1);
+	if (!buf1)
 		return (NULL);
-	*buf1		= '\0';
+	*buf1 = '\0';
 	while (!ft_strchr(buf1, '\n'))
 	{
-		if ((read_ret = read(fd, buf1, BUFFER_SIZE)) <= 0)
+		read_ret = read(fd, buf1, BUFFER_SIZE);
+		if ((read_ret <= 0))
 			break ;
 		buf1[read_ret] = '\0';
 		buf2 = ft_strcjoin(raw_line, buf1, '\0');
@@ -83,11 +86,12 @@ char	*get_next_line(int fd)
 	char		*raw_line;
 	char		*line;
 
-	if ((line = check_save(save)) != NULL)
+	line = check_save(save);
+	if (line != NULL)
 		return (line);
 	raw_line = ft_read_buffer(fd);
 	line = gnl_split(raw_line, save);
-	free(buf);
+	free(raw_line);
 	return (line);
 }
 #endif
